5c5ad00a4cd0e7b7209f1f1e8899c8bf1ce0f315
Wallace <wallacegomes6877@gmail.com>
2025-02-20 16:56:10 -0300

    feat:Nav component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './src/Components/header/header.component';
+import { NavComponent } from "./src/Components/nav/nav.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
