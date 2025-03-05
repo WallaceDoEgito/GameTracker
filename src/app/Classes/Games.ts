93029af4dd8471e99026a566e6cb841960d23f62
Wallace <wallacegomes6877@gmail.com>
2025-03-05 16:51:30 -0300

    fix:Game class now has all fields needed

--- src/app/Classes/Games.ts
+++ src/app/Classes/Games.ts
@@ -3,12 +3,14 @@ export class Game {
     gameName:string;
     gameImageUrl:string;
     hoursPlayed:number;
+    review:string;
     rating:number;
-    constructor(id:number,name:string,url:string,time:number,rating:number) {
+    constructor(id:number,name:string,url:string,time:number,review:string, rating:number) {
         this.gameId = id
         this.gameName = name
         this.gameImageUrl = url
         this.hoursPlayed = time
+        this.review = review
         this.rating = rating
 
         
