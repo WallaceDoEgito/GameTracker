1f5260ac4031f0f0c6d8686d0b8c594e8669185c
Wallace <wallacegomes6877@gmail.com>
2025-03-08 16:53:57 -0300

    feat:style for dialog popup

--- src/app/src/Components/add-game-dialog-pop-up/add-game-dialog-pop-up.component.ts
+++ src/app/src/Components/add-game-dialog-pop-up/add-game-dialog-pop-up.component.ts
@@ -1,10 +1,11 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import {OutsideClickDirective} from '../../Directives/outside-click.directive'
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-add-game-dialog-pop-up',
-  imports: [FormsModule, OutsideClickDirective],
+  imports: [FormsModule, OutsideClickDirective, NgOptimizedImage ],
   templateUrl: './add-game-dialog-pop-up.component.html',
   styleUrl: './add-game-dialog-pop-up.component.css'
 })
@@ -27,6 +28,5 @@ export class AddGameDialogPopUpComponent {
   }
 
   SearchName(){
-
   }
 }
