886b97882b9eaf4c0d0d77b95b9c29d412ce3a9c
Wallace <wallacegomes6877@gmail.com>
2025-03-12 21:11:48 -0300

    feat:menu options button

--- src/app/src/Components/game/game.component.ts
+++ src/app/src/Components/game/game.component.ts
@@ -1,13 +1,20 @@
 import { Component, Input, input } from '@angular/core';
 import { Game } from '../../../Classes/Games';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-game',
-  imports: [],
+  imports: [MatMenuModule, MatButtonModule, MatIconModule],
   templateUrl: './game.component.html',
   styleUrl: './game.component.css'
 })
 export class GameComponent {
   @Input() gameInput!:Game
+
+  OpenMenu(){
+
+  }
 
 }
