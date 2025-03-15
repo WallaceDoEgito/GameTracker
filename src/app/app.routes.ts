902ff7c036fe2af5f7fafd5c2146ff9cb04804f3
Wallace <wallacegomes6877@gmail.com>
2025-03-15 10:19:04 -0300

    feat:redirection if try to acess without being redirect by main page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { GameMoreDetailComponent } from './src/Components/game-more-detail/game-more-detail.component';
+import { GameMoreDetailComponent } from './src/Pages/game-more-detail/game-more-detail.component';
 import { GameMainPageComponent } from './src/Pages/game-main-page/game-main-page.component';
 
 export const routes: Routes = [
--- src/app/src/Pages/game-more-detail/game-more-detail.component.ts
+++ src/app/src/Pages/game-more-detail/game-more-detail.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input, OnInit } from '@angular/core';
+import { ApiService } from '../../Services/api.service';
+import { Game } from '../../../Classes/Games';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-game-more-detail',
@@ -6,6 +9,23 @@ import { Component } from '@angular/core';
   templateUrl: './game-more-detail.component.html',
   styleUrl: './game-more-detail.component.css'
 })
-export class GameMoreDetailComponent {
+export class GameMoreDetailComponent implements OnInit {
+  @Input() id!:number
+  private ApiCalls = inject(ApiService)
+  private router = inject(Router)
+  public GameSelected?:Game
+  ngOnInit(){
+    let gamelist:Game[] = this.ApiCalls.getList()
+    this.GameSelected = this.FindGameById(gamelist, this.id)
+    if(this.GameSelected == undefined) this.router.navigate([''])
+  }
 
+  private FindGameById(list:Game[], id:number)
+  {
+    for(let game of list){
+      console.log(game.gameId)
+      if(game.gameId == id) return game
+    }
+    return undefined
+  }
 }
