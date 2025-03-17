4b959891c59bcbf4ec2f33908ca2621360cfc0b0
Wallace <wallacegomes6877@gmail.com>
2025-03-17 19:57:35 -0300

    feat:back button on page 2

--- Frontend/src/app/src/Pages/game-more-detail/game-more-detail.component.ts
+++ Frontend/src/app/src/Pages/game-more-detail/game-more-detail.component.ts
@@ -2,10 +2,12 @@ import { Component, inject, Input, OnInit } from '@angular/core';
 import { ApiService } from '../../Services/api.service';
 import { Game } from '../../../Classes/Games';
 import { Router } from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-game-more-detail',
-  imports: [],
+  imports: [MatIconModule, MatButtonModule],
   templateUrl: './game-more-detail.component.html',
   styleUrl: './game-more-detail.component.css'
 })
@@ -23,9 +25,12 @@ export class GameMoreDetailComponent implements OnInit {
   private FindGameById(list:Game[], id:number)
   {
     for(let game of list){
-      console.log(game.gameId)
       if(game.gameId == id) return game
     }
     return undefined
+  }
+
+  public BackToHomePage(){
+    this.router.navigate([''])
   }
 }
