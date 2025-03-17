068d5fb1cb3bca773fd6a6c36d4535f2b6e664e1
Wallace <wallacegomes6877@gmail.com>
2025-03-17 12:57:04 -0300

    feat:mssql script wait for sqlserver accept connections

--- Mssql/configure-db.sh
+++ Mssql/configure-db.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-sleep 20s
+wait_for_sql_server() {
+  echo "Aguardando o SQL Server iniciar..."
+  while ! /opt/mssql-tools18/bin/sqlcmd -C -S localhost -U sa -P "$MSSQL_SA_PASSWORD" -Q "SELECT 1" > /dev/null 2>&1; do
+    echo "SQL Server não está pronto ainda. Aguardando..."
+    sleep 5
+  done
+  echo "SQL Server está pronto!"
+}
+
+wait_for_sql_server
+
 echo "Configurando a database"
 /opt/mssql-tools18/bin/sqlcmd -C -S localhost -U sa -P "$MSSQL_SA_PASSWORD" -d master -i /usr/config/setup.sql
--- Mssql/entrypoint.sh
+++ Mssql/entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-# Start the script to create the DB and user
 /opt/mssql/bin/sqlservr & SQL_PID=$! & /usr/config/configure-db.sh
 wait $SQL_PID
