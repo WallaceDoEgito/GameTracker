7dffd400723579945ae7825e99f91e967ea72dea
Wallace <wallacegomes6877@gmail.com>
2025-03-29 20:44:50 -0300

    fix:constructor for dapper errors

--- Backend/Model/TimeSession.cs
+++ Backend/Model/TimeSession.cs
@@ -1,9 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
 namespace JogosDoAno.Model
 {
     public class TimeSession
     {
-        public int GameId { get; set; }
-        public DateOnly SessionDate {get; set;}
-        public TimeOnly SessionHours {get; set;}
+
+        [Required]
+        public DateOnly SessionDate 
+        {
+            get;
+            set;
+        }
+        [Required]
+        public TimeOnly SessionHours
+        {
+            get;
+            set;
+        }
+
+        public TimeSession(DateTime sessionDate, TimeSpan sessionHours)
+        {
+            SessionDate = DateOnly.FromDateTime(sessionDate);
+            SessionHours = TimeOnly.FromTimeSpan(sessionHours);
+
+        }
+        public TimeSession(){}
     }
 }
