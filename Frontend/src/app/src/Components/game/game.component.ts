fa9ca9674e5db8197d92fb8be96db381bc44ca64
Wallace <wallacegomes6877@gmail.com>
2025-04-05 16:08:39 -0300

    fix:home pages now calculates the total time right

--- Frontend/src/app/src/Components/game/game.component.ts
+++ Frontend/src/app/src/Components/game/game.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, inject, Input, Output} from '@angular/core';
+import { Component, EventEmitter, inject, Input, OnInit, Output} from '@angular/core';
 import { Game } from '../../../Classes/Games';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatButtonModule} from '@angular/material/button';
@@ -6,6 +6,8 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatDialog} from '@angular/material/dialog';
 import { ConfirmationDialogComponent } from '../Dialogs/confirmation-dialog/confirmation-dialog.component';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { ApiService } from '../../Services/api.service';
+import { SessionTime } from '../../../Classes/SessionTime';
 
 @Component({
   selector: 'app-game',
@@ -13,10 +15,29 @@ import { RouterLink, RouterOutlet } from '@angular/router';
   templateUrl: './game.component.html',
   styleUrl: './game.component.css'
 })
-export class GameComponent {
+export class GameComponent implements OnInit{
   @Input() gameInput!:Game
   @Output() deleteGameEvent = new EventEmitter<Game>()
   dialog = inject(MatDialog)
+  apiCall = inject(ApiService)
+  sessions! : SessionTime[]
+
+  totalTime = 0 
+
+  async ngOnInit() {
+      this.sessions = await this.apiCall.getSession(this.gameInput.gameId)
+      this.CalcTotalTime();
+  }
+
+  private CalcTotalTime() : void{
+    let totalSum = this.gameInput!.hoursPlayed;
+    for(let g of this.sessions!){
+      totalSum += Number.parseInt(g.sessionHours.slice(0,2))
+      totalSum += Number.parseFloat(g.sessionHours.slice(3,5)) / 60
+    }
+    this.totalTime = totalSum;
+  }
+  
 
   OpenDialogConfirmation(){
     const dialogRef = this.dialog.open(ConfirmationDialogComponent);
